449429a4b13d1e68931b6ebf099029c394f2523c
Jerônimo <karkwogvoldor@gmail.com>
2026-05-12 13:33:19 -0300

    alterando o html usando o forms.py

---
+++ usuarios/forms.py
@@ -0,0 +1,77 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label="Nome de Login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )    
+    )
+
+
+    senha = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    
+    
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome Completo",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
+    )
+
+    email_cadastro = forms.CharField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: joaosilva@gmail.com"
+            }
+        )
+    )
+
+    senha_cadastro = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Digite sua senha"
+            }
+        )
+    )
+
+    confirmar_senha_cadastro = forms.CharField(
+        label="Confirmação de Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Confirme sua senha"
+            }
+        )
+    )
